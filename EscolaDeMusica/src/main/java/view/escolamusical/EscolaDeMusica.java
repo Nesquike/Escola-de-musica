ac0675bcf84f520e012a9c83f965c3a815630869
luiz.gbrito <luiz.gbrito@041ET128075.catolica.ubec>
2025-10-07 10:54:21 -0300

    Criação da classe main

---
+++ EscolaDeMusica/src/main/java/view/escolamusical/EscolaDeMusica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package view.escolamusical;
+
+/**
+ *
+ * @author luiz.gbrito
+ */
+public class EscolaDeMusica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
